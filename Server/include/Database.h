cc39a68e8ab36cad304331e1d17dbab9ec3150d2
aziegart <olaziegart@gmail.com>
2016-05-30 17:49:35 +0200

    #6 Baza danych

---
+++ Server/include/Database.h
@@ -0,0 +1,47 @@
+//
+// Created by Aleksandra Ziegart on 30/05/16.
+//
+
+#ifndef PROJEKTTIN_DATABASE_H
+#define PROJEKTTIN_DATABASE_H
+
+
+#include <sqlite3.h>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <cstring>
+#include <sstream>
+
+
+
+using namespace std;
+
+class Database
+{
+public:
+    Database();
+    ~Database();
+
+    sqlite3 *db;
+    char *zErrMsg;
+    int rc;
+    const char *sql;
+    const char* data;
+    string sql_temp;
+    ostringstream oss;
+
+    bool open();
+    void close();
+    bool create_table();
+    int insert(int id_machine, int id_measurement, string status, int port, int alarm_value, int current_value);
+    bool select_all();
+    bool delete_row(int id_machine, int id_measurement);
+    bool update(string status, int id_machine, int id_measurement);
+    static int callback(void *NotUsed, int argc, char **argv, char **azColName);
+
+
+};
+
+
+#endif //PROJEKTTIN_DATABASE_H
