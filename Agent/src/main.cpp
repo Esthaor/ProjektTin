c694f5dcb0a0238e24c0aece4bf03d01a143e6c7
lipsztyk <M.A.Lipinski@stud.elka.pw.edu.pl>
2016-05-31 01:19:09 +0200

    Uzupełnienie zatrzymywania oraz zmiany pomiaru o pomiar z "palca".

--- Agent/src/main.cpp
+++ Agent/src/main.cpp
@@ -63,7 +63,7 @@ int main(int argc, char* argv[]) {
     if(argc > 1) {
         Agent *agent = new Agent(socket->next_capture_id, port, end_condition, end_condition_value, alarm);
         socket->thread_list.push_back(new boost::thread(boost::bind(&Agent::sniff, agent)));
-        socket->thread_list[socket->next_capture_id]->detach();
+        socket->mutex_list.push_back(new ThreadMutex(socket->next_capture_id));
         socket->next_capture_id++;
         string dowyslania = agent->buildJson("started");
         cout << "Taki zaraz powinien wyslac " << dowyslania << endl;
