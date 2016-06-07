4b507f919b82ec7582f64b19ef3b4ea4469da5c3
Grzegorz Majchrzak <gregiem94@gmail.com>
2016-06-07 14:35:08 +0200

    serwer www almost done

--- Server/include/WwwServer.h
+++ Server/include/WwwServer.h
@@ -18,7 +18,10 @@ public:
     ~WwwServer(){};
     int wwwServerStart();
     static void handle_print_list(struct mg_connection *nc, struct http_message *hm);
-    static void handle_sum_call(struct mg_connection *nc, struct http_message *hm);
+    static void handle_add_measurment(struct mg_connection *nc, struct http_message *hm);
+    static void handle_add_machine(struct mg_connection *nc, struct http_message *hm);
+    static void handle_end_measurement(struct mg_connection *nc, struct http_message *hm);
+    static void handle_edit_measurement(struct mg_connection *nc, struct http_message *hm);
     static void ev_handler(struct mg_connection *nc, int ev, void *ev_data);
 };
 
